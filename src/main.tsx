aa01056507cffcbf7e20863f846633b4feffd854
김진영 <skswls0@daum.net>
2023-07-16 23:57:25 +0900

    react-query 기본 설정 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "@material-tailwind/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import routes from "@/routes";
 import "@/styles/main.css";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,7 +17,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <RecoilRoot>
-        <RouterProvider router={routes} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={routes} />
+        </QueryClientProvider>
       </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>
