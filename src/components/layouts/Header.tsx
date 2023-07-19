eaad8eabca4985e0ec746b6b2ee1640420131b71
김진영 <skswls0@daum.net>
2023-07-19 16:22:18 +0900

    헤더 메뉴 항목에 todo 페이지 추가

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -10,7 +10,12 @@ export function Header() {
   return (
     <header className="fixed w-full  top-0 left-0 right-0 z-50 shadow-sm h-[72px] bg-gray-100">
       <div className="flex items-center justify-between max-w-[1440px] mx-auto h-full px-4">
-        <Link to="/">Home</Link>
+        <div className="flex">
+          <Link to="/" className="mr-24">
+            Home
+          </Link>
+          <Link to="/todos">Todo</Link>
+        </div>
         <Link to="/login">
           <button>로그인</button>
         </Link>
