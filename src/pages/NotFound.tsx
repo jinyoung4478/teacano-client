a4f4415d072e43b57a4f30c6ced1fdcd03ffdf6b
김진영 <skswls0@daum.net>
2023-07-19 16:11:10 +0900

    404 페이지에 복귀 링크 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
     <div className="w-full flex flex-col justify-center items-center p-20 gap-4">
       <p className="text-2xl font-bold">Page Not Found</p>
-      <p className="text-2xl">( ᵕ ̯ ᵕ̩̩ )</p>
+      <p className="text-2xl mb-4">( ᵕ ̯ ᵕ̩̩ )</p>
+      <Link
+        to={"/"}
+        className="text-blue-300 hover:text-blue-600 hover:underline"
+      >
+        Go to Homepage
+      </Link>
     </div>
   );
 }
