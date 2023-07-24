5f0d3233f0c3544ec95bf983bb1fc94e9dca8ae3
Harry K <skswls0@daum.net>
2023-07-24 17:04:26 +0900

    Header 스타일 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,19 @@
 import { Link } from "react-router-dom";
 
+interface IHeaderItemProps {
+  size?: "sm" | "md" | "lg";
+  text: string;
+  className?: string;
+}
+
+const HeaderItem = ({ size = "md", text, className }: IHeaderItemProps) => {
+  return (
+    <button className={`text-gray-800 text-${size} ${className}`}>
+      {text}
+    </button>
+  );
+};
+
 export const Header = () => {
   return (
     <header className="fixed w-full  top-0 left-0 right-0 z-50 shadow-sm h-[72px]">
@@ -9,9 +23,27 @@ export const Header = () => {
             Home
           </Link>
         </div>
-        <Link to="/login">
-          <button>로그인</button>
-        </Link>
+        <div className="flex gap-8">
+          <Link to="/info">
+            <HeaderItem size={"lg"} text={"티카노란?"} />
+          </Link>
+          <Link to="/market">
+            <HeaderItem size={"lg"} text={"원두 구매하기"} />
+          </Link>
+        </div>
+        <div className="flex gap-2">
+          <Link to="/join">
+            <HeaderItem size={"sm"} text={"회원가입"} />
+          </Link>
+          <HeaderItem size={"sm"} text={"|"} className="text-thin" />
+          <Link to="/login">
+            <HeaderItem size={"sm"} text={"로그인"} />
+          </Link>
+          <HeaderItem size={"sm"} text={"|"} className="text-thin" />
+          <Link to="/cart">
+            <HeaderItem size={"sm"} text={"장바구니"} />
+          </Link>
+        </div>
       </div>
     </header>
   );
