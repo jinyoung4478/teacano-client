4b5f32a6dd177dee677d6516a4a3088a597c06e1
Harry K <skswls0@daum.net>
2023-07-24 16:47:10 +0900

    브랜드 컬러 등의 css 설정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="fixed w-full  top-0 left-0 right-0 z-50 shadow-sm h-[72px] bg-gray-100">
+    <header className="fixed w-full  top-0 left-0 right-0 z-50 shadow-sm h-[72px]">
       <div className="flex items-center justify-between max-w-[1440px] mx-auto h-full px-4">
         <div className="flex gap-8">
           <Link to="/" className="mr-24">
